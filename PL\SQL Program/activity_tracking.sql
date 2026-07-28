c835391969b7615b4f7a57219a7cb56f4a8ff27f
Shedrick Duanah <duanahs@gmail.com>
2026-07-28 17:26:44 +0200

    Create activity_tracking.sql

---
+++ PL\SQL Program/activity_tracking.sql
@@ -0,0 +1,28 @@
+CREATE TABLE user_activity
+(
+    activity_id NUMBER PRIMARY KEY,
+    username VARCHAR2(50),
+    login_time DATE
+);
+
+CREATE SEQUENCE activity_seq
+START WITH 1
+INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER trg_logon
+AFTER LOGON
+ON DATABASE
+
+BEGIN
+
+    INSERT INTO user_activity
+
+    VALUES
+    (
+        activity_seq.NEXTVAL,
+        USER,
+        SYSDATE
+    );
+
+END;
+/
