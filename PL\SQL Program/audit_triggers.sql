e0b5af477b0607b7f989582f670c2232f209a215
Shedrick Duanah <duanahs@gmail.com>
2026-07-29 14:23:14 +0200

    Implement auditing triggers for stock_batches and dispensing
    Added auditing triggers for stock_batches and dispensing tables to log changes in the audit_log table.

--- PL\SQL Program/audit_triggers.sql
+++ PL\SQL Program/audit_triggers.sql
@@ -1,16 +1,21 @@
--- Goes in: PL\SQL Program/audit_triggers.sql
 CREATE SEQUENCE audit_seq START WITH 1 INCREMENT BY 1 NOCACHE NOCYCLE;
 
 CREATE OR REPLACE TRIGGER trg_audit_drugs
 AFTER INSERT OR UPDATE OR DELETE ON drugs
 FOR EACH ROW
+DECLARE
+    v_op VARCHAR2(10);
 BEGIN
+    IF INSERTING THEN v_op := 'INSERT';
+    ELSIF UPDATING THEN v_op := 'UPDATE';
+    ELSE v_op := 'DELETE';
+    END IF;
+
     INSERT INTO audit_log (
         audit_id, table_name, operation, record_id,
         old_value, new_value, changed_by, change_date
     ) VALUES (
-        audit_seq.NEXTVAL, 'DRUGS',
-        CASE WHEN INSERTING THEN 'INSERT' WHEN UPDATING THEN 'UPDATE' WHEN DELETING THEN 'DELETE' END,
+        audit_seq.NEXTVAL, 'DRUGS', v_op,
         NVL(:NEW.drug_id, :OLD.drug_id),
         TO_CHAR(:OLD.drug_name), TO_CHAR(:NEW.drug_name),
         USER, SYSDATE
@@ -18,18 +23,22 @@ BEGIN
 END;
 /
 
--- added: STOCK_BATCHES and DISPENSING were previously unaudited,
--- even though they're the most business-critical tables
 CREATE OR REPLACE TRIGGER trg_audit_stock_batches
 AFTER INSERT OR UPDATE OR DELETE ON stock_batches
 FOR EACH ROW
+DECLARE
+    v_op VARCHAR2(10);
 BEGIN
+    IF INSERTING THEN v_op := 'INSERT';
+    ELSIF UPDATING THEN v_op := 'UPDATE';
+    ELSE v_op := 'DELETE';
+    END IF;
+
     INSERT INTO audit_log (
         audit_id, table_name, operation, record_id,
         old_value, new_value, changed_by, change_date
     ) VALUES (
-        audit_seq.NEXTVAL, 'STOCK_BATCHES',
-        CASE WHEN INSERTING THEN 'INSERT' WHEN UPDATING THEN 'UPDATE' WHEN DELETING THEN 'DELETE' END,
+        audit_seq.NEXTVAL, 'STOCK_BATCHES', v_op,
         NVL(:NEW.batch_id, :OLD.batch_id),
         TO_CHAR(:OLD.quantity_available), TO_CHAR(:NEW.quantity_available),
         USER, SYSDATE
@@ -40,13 +49,19 @@ END;
 CREATE OR REPLACE TRIGGER trg_audit_dispensing
 AFTER INSERT OR UPDATE OR DELETE ON dispensing
 FOR EACH ROW
+DECLARE
+    v_op VARCHAR2(10);
 BEGIN
+    IF INSERTING THEN v_op := 'INSERT';
+    ELSIF UPDATING THEN v_op := 'UPDATE';
+    ELSE v_op := 'DELETE';
+    END IF;
+
     INSERT INTO audit_log (
         audit_id, table_name, operation, record_id,
         old_value, new_value, changed_by, change_date
     ) VALUES (
-        audit_seq.NEXTVAL, 'DISPENSING',
-        CASE WHEN INSERTING THEN 'INSERT' WHEN UPDATING THEN 'UPDATE' WHEN DELETING THEN 'DELETE' END,
+        audit_seq.NEXTVAL, 'DISPENSING', v_op,
         NVL(:NEW.dispensing_id, :OLD.dispensing_id),
         TO_CHAR(:OLD.quantity_dispensed), TO_CHAR(:NEW.quantity_dispensed),
         USER, SYSDATE
