a0437cbe558b49d34a9708ef793af94be2ce9782
Shedrick Duanah <duanahs@gmail.com>
2026-07-29 20:40:14 +0200

    Update audit_triggers.sql

--- PL\SQL Program/audit_triggers.sql
+++ PL\SQL Program/audit_triggers.sql
@@ -1,70 +1,26 @@
-CREATE SEQUENCE audit_seq START WITH 1 INCREMENT BY 1 NOCACHE NOCYCLE;
-
-CREATE OR REPLACE TRIGGER trg_audit_drugs
-AFTER INSERT OR UPDATE OR DELETE ON drugs
+CREATE OR REPLACE TRIGGER TRG_AUDIT_STOCK_BATCHES
+AFTER INSERT OR UPDATE OR DELETE ON STOCK_BATCHES
 FOR EACH ROW
-DECLARE
-    v_op VARCHAR2(10);
 BEGIN
-    IF INSERTING THEN v_op := 'INSERT';
-    ELSIF UPDATING THEN v_op := 'UPDATE';
-    ELSE v_op := 'DELETE';
-    END IF;
 
-    INSERT INTO audit_log (
-        audit_id, table_name, operation, record_id,
-        old_value, new_value, changed_by, change_date
-    ) VALUES (
-        audit_seq.NEXTVAL, 'DRUGS', v_op,
-        NVL(:NEW.drug_id, :OLD.drug_id),
-        TO_CHAR(:OLD.drug_name), TO_CHAR(:NEW.drug_name),
-        USER, SYSDATE
+    INSERT INTO AUDIT_LOG
+    (
+        AUDIT_ID,
+        USERNAME,
+        ACTION_TYPE,
+        TABLE_NAME,
+        ACTION_DATE,
+        DESCRIPTION
+    )
+    VALUES
+    (
+        AUDIT_SEQ.NEXTVAL,
+        USER,
+        'STOCK_BATCH_CHANGE',
+        'STOCK_BATCHES',
+        SYSTIMESTAMP,
+        'Stock batch record modified'
     );
-END;
-/
 
-CREATE OR REPLACE TRIGGER trg_audit_stock_batches
-AFTER INSERT OR UPDATE OR DELETE ON stock_batches
-FOR EACH ROW
-DECLARE
-    v_op VARCHAR2(10);
-BEGIN
-    IF INSERTING THEN v_op := 'INSERT';
-    ELSIF UPDATING THEN v_op := 'UPDATE';
-    ELSE v_op := 'DELETE';
-    END IF;
-
-    INSERT INTO audit_log (
-        audit_id, table_name, operation, record_id,
-        old_value, new_value, changed_by, change_date
-    ) VALUES (
-        audit_seq.NEXTVAL, 'STOCK_BATCHES', v_op,
-        NVL(:NEW.batch_id, :OLD.batch_id),
-        TO_CHAR(:OLD.quantity_available), TO_CHAR(:NEW.quantity_available),
-        USER, SYSDATE
-    );
-END;
-/
-
-CREATE OR REPLACE TRIGGER trg_audit_dispensing
-AFTER INSERT OR UPDATE OR DELETE ON dispensing
-FOR EACH ROW
-DECLARE
-    v_op VARCHAR2(10);
-BEGIN
-    IF INSERTING THEN v_op := 'INSERT';
-    ELSIF UPDATING THEN v_op := 'UPDATE';
-    ELSE v_op := 'DELETE';
-    END IF;
-
-    INSERT INTO audit_log (
-        audit_id, table_name, operation, record_id,
-        old_value, new_value, changed_by, change_date
-    ) VALUES (
-        audit_seq.NEXTVAL, 'DISPENSING', v_op,
-        NVL(:NEW.dispensing_id, :OLD.dispensing_id),
-        TO_CHAR(:OLD.quantity_dispensed), TO_CHAR(:NEW.quantity_dispensed),
-        USER, SYSDATE
-    );
 END;
 /
